0d60732d51215ff0b9363d3b7388a9836b8aa95c
Blackheer <pffjij@gmail.com>
2025-09-09 16:02:28 +0200

    hallo henk het bewijs

--- content/quoter_templates.py
+++ content/quoter_templates.py
@@ -75,7 +75,7 @@ def page(content,user_id,title,error=None):
 
 <header>
   <div class="title">
-    <a class="home" href="/">Quoter XP535961</a>
+    <a class="home" href="/">Quoter XP535961 Dag Henk ben je nu tevreden</a>
     {links}
   </div>
 </header>
